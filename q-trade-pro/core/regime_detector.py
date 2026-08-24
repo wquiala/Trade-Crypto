e222c25ea7c953c36683ce7ba2c67b8d548fb47b
wquiala <wilfredoquiala@gmail.com>
2026-08-24 22:05:27 +0200

    Hacer el bot más agresivo bajando el umbral de ADX

--- q-trade-pro/core/regime_detector.py
+++ q-trade-pro/core/regime_detector.py
@@ -30,11 +30,11 @@ class RegimeDetector:
         # Podría implementarse con percentiles históricos de ATR
         
         # 2. Ranging (Mercado Lateral)
-        if adx < 20:
+        if adx < 18:
             return 'RANGING'
             
         # 3. Tendencias
-        if adx >= 25:
+        if adx >= 20:
             if close > ema50 and ema50 > ema200:
                 return 'BULL_TREND'
             elif close < ema50 and ema50 < ema200:
